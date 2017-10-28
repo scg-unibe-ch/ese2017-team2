7e1fa3ccb2803f7d21e2965f06b47e60cb6ef937
Jan Eich <jan.eich@students.unibe.ch>
2017-10-28 22:35:02 +0200

    Update example database

--- example database/ese.sql
+++ example database/ese.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Erstellungszeit: 27. Okt 2017 um 21:47
+-- Erstellungszeit: 28. Okt 2017 um 22:28
 -- Server-Version: 10.1.26-MariaDB
 -- PHP-Version: 7.1.9
 
@@ -12,15 +12,34 @@ SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Datenbank: `ese`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `authorities`
+--
+
+CREATE TABLE `authorities` (
+  `username` varchar(50) NOT NULL,
+  `authority` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `authorities`
+--
+
+INSERT INTO `authorities` (`username`, `authority`) VALUES
+('admin', 'ROLE_ADMIN'),
+('Anja Müller', 'ROLE_DRIVER'),
+('Dana Baumann', 'ROLE_DRIVER'),
+('Hannes Kraus', 'ROLE_DRIVER'),
+('Kai Wagner', 'ROLE_DRIVER'),
+('Kristina Becker', 'ROLE_DRIVER'),
+('Simon Müller', 'ROLE_DRIVER'),
+('Sven Weber', 'ROLE_DRIVER');
 
 -- --------------------------------------------------------
 
@@ -30,22 +49,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `driver` (
   `id` bigint(20) NOT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL
+  `name` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Daten für Tabelle `driver`
 --
 
-INSERT INTO `driver` (`id`, `name`, `password`) VALUES
-(1, 'Simon Müller', '1234'),
-(2, 'Sven Weber', 'abcd'),
-(3, 'Kai Wagner', '4321'),
-(4, 'Hannes Kraus', 'dcba'),
-(5, 'Anja Müller', 'anja12'),
-(6, 'Kristina Becker', 'kb1234'),
-(7, 'Dana Baumann', 'db12');
+INSERT INTO `driver` (`id`, `name`) VALUES
+(1, 'Simon Müller'),
+(2, 'Sven Weber'),
+(3, 'Kai Wagner'),
+(4, 'Hannes Kraus'),
+(5, 'Anja Müller'),
+(6, 'Kristina Becker'),
+(7, 'Dana Baumann');
 
 -- --------------------------------------------------------
 
@@ -83,9 +101,41 @@ INSERT INTO `trip` (`id`, `animal`, `animal_count`, `city_1`, `city_2`, `custome
 (6, 'goat', 10, 'Basel', 'Biel', 'Karl Bergmann', '2017-11-30 10:00:00', 'Nele Ludwig', 'Karl Bergmann', 4057, 2502, 'Feldbergstrasse 2', 'Neumarktstrasse 32', 0, NULL),
 (7, 'deer', 2, 'Biel', 'Zürich', 'Karolina Fischer', '2017-10-27 16:15:00', 'Noah Koch', 'Karolina Fsicher', 2502, 8008, 'Gartenstrasse 12', 'Krezstrasse 22', 3, 7);
 
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `users`
+--
+
+CREATE TABLE `users` (
+  `username` varchar(50) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `enabled` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Daten für Tabelle `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `enabled`) VALUES
+('admin', 'anitrans', 1),
+('Anja Müller', 'anja12', 1),
+('Dana Baumann', 'db12', 1),
+('Hannes Kraus', 'dcba', 1),
+('Kai Wagner', '4321', 1),
+('Kristina Becker', 'kb1234', 1),
+('Simon Müller', '1234', 1),
+('Sven Weber', 'abcd', 1);
+
 --
 -- Indizes der exportierten Tabellen
 --
+
+--
+-- Indizes für die Tabelle `authorities`
+--
+ALTER TABLE `authorities`
+  ADD UNIQUE KEY `ix_auth_username` (`username`,`authority`);
 
 --
 -- Indizes für die Tabelle `driver`
@@ -99,6 +149,12 @@ ALTER TABLE `driver`
 ALTER TABLE `trip`
   ADD PRIMARY KEY (`id`),
   ADD KEY `FKpuhkx68hnwy4by2b0onybv5x1` (`driver_id`);
+
+--
+-- Indizes für die Tabelle `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
@@ -121,12 +177,14 @@ ALTER TABLE `trip`
 --
 
 --
+-- Constraints der Tabelle `authorities`
+--
+ALTER TABLE `authorities`
+  ADD CONSTRAINT `fk_authorities_users` FOREIGN KEY (`username`) REFERENCES `users` (`username`);
+
+--
 -- Constraints der Tabelle `trip`
 --
 ALTER TABLE `trip`
   ADD CONSTRAINT `FKpuhkx68hnwy4by2b0onybv5x1` FOREIGN KEY (`driver_id`) REFERENCES `driver` (`id`);
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
